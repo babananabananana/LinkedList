cfd7aa2b315d34586d167664864b08028f95c31a
babananabananana <babananabananana@outlook.com>
2020-02-27 17:14:35 -0700

    Initial commit

---
+++ LinkedList.h
@@ -0,0 +1,14 @@
+#ifndef LINKEDLIST_LINKEDLIST_H
+#define LINKEDLIST_LINKEDLIST_H
+#include "LinkedListInterface.h"
+
+using namespace std;
+
+class LinkedList : LinkedListInterface{
+private:
+    int myNum = 6;
+
+
+};
+
+#endif //LINKEDLIST_LINKEDLIST_H
